a14afd95174a76c6c6b7ab5429a0ff69796a9ad4
Austral-marcos <mbrunettaalvarezper@mail.austral.edu.ar>
2025-03-21 12:38:12 -0300

    Update exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -1,9 +1,9 @@
 def math():
     a, b = 57, 7
-print(a+b)
-print(a-b)
-print(a*b)
-print((a+b)/2)
-print(a//b)
-print(a%b)
-print(a/b)
+    print(a+b)
+    print(a-b)
+    print(a*b)
+    print((a+b)/2)
+    print(a//b)
+    print(a%b)
+    print(a/b)
